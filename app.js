cae4c9527283fadbe734b9ed47a67117b5754a18
sandeep2630 <katwal.sandeep28@gmail.com>
2023-05-11 11:57:01 +0530

    fs module

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("hello");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.write("welcome to my home page");
+  res.end();
+});
+server.listen(5000);
